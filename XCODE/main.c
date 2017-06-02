b971d08260f981c34e896120517ec319e77d7329
Gyu hyun Seo <gyuhyunseo@Gyuui-MacBook-Pro.local>
2017-06-02 15:58:57 +0900

    Initial Commit

---
+++ XCODE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  XCODE
+//
+//  Created by Gyu hyun Seo on 2017. 6. 2..
+//  Copyright © 2017년 Gyu hyun Seo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
